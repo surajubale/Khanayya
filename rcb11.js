db98b244e63488f17f505f1a83f5133ae5dab8e9
surajubale <surajubale97@gmail.com>
2021-10-09 14:08:50 +0530

    RCB 11 is added

---
+++ rcb11.js
@@ -0,0 +1,17 @@
+let choice = 3;
+switch (choice) {
+    case 1:
+    case 2:
+        choice = 'Virat, Dev';
+        break;
+    case 3:
+    case 4:
+        choice = 'ABD , Maxi';
+        break;
+    case 5:
+        choice = 'K.S Bharat'
+        break;
+    default:
+        choice = 'no batsman left'
+}    
+console.log(choice)
