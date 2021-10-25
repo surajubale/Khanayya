48f4c9d4faddf454074b6211f8cc80632f4f6691
surajubale <surajubale97@gmail.com>
2021-10-25 14:47:32 +0530

    try

---
+++ oopsoops.js
@@ -0,0 +1,16 @@
+var employee = new Object();
+employee.name = 'Girish';
+employee.id = '33555';
+employee.salary = 90000;
+employee.mailId =  'girishsunad@yahoo.com';
+employee.Detail = function() {
+    console.log(this.name, this.id, this.salary, this.mailId);
+},
+employee.location = ' hubli ';
+employee.cluster = function() {
+    console.log(this.location + ' NorthKarnataka ');
+};
+
+employee.Detail();
+employee.cluster();
+console.log(employee.Detail());
