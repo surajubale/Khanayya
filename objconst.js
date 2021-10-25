cd6f91ac0f2d9d90414580b463e0f73f973b3c05
surajubale <surajubale97@gmail.com>
2021-10-25 14:31:53 +0530

    emplyee cluster is added

---
+++ objconst.js
@@ -0,0 +1,15 @@
+var employee = new Object();
+employee.name = 'Girish';
+employee.id = '33555';
+employee.salary = 90000;
+employee.mailId =  'girishsunad@yahoo.com';
+employee.Detail = function() {
+    console.log(this.name, this.id, this.salary, this.mailId);
+},
+employee.location = ' hubli ';
+employee.cluster = function() {
+    console.log(this.location + ' NorthKarnataka ');
+};
+
+employee.Detail();
+employee.cluster();
